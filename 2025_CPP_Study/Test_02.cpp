68cadbf402852691426269bdd77e70d79bdd6f17
Najisu <s2322@e-mirim.hs.kr>
2025-04-07 23:54:44 +0900

    using namespace 사용 후

--- 2025_CPP_Study/Test_02.cpp
+++ 2025_CPP_Study/Test_02.cpp
@@ -1,10 +1,11 @@
 // °ªÀ» Ãâ·ÂÇÏ´Â ¿¹Á¦
 #include <iostream>
+using namespace std;
 
 void main() {
-	std::cout << "cout °´Ã¼¸¦ ÀÌ¿ëÇÑ Ãâ·Â" << std::endl;
-	std::cout << "µ¡¼À : 10 + 6 = " << 10 + 6 << std::endl;
-	std::cout << "»¬¼À : 8 - 3 = " << 8 - 3 << std::endl;
-	std::cout << "°ö¼À : 4 * 5 = " << 4 * 5 << std::endl;
-	std::cout << "³ª´°¼À : 3 / 2 = " << (double)3 / 2 << std::endl;
+	cout << "cout °´Ã¼¸¦ ÀÌ¿ëÇÑ Ãâ·Â" << endl;
+	cout << "µ¡¼À : 10 + 6 = " << 10 + 6 << endl;
+	cout << "»¬¼À : 8 - 3 = " << 8 - 3 << endl;
+	cout << "°ö¼À : 4 * 5 = " << 4 * 5 << endl;
+	cout << "³ª´°¼À : 3 / 2 = " << (double)3 / 2 << endl;
 }

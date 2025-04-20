d2d168026b7dcd4a1923d152339b5064d26555c1
Najisu <s2322@e-mirim.hs.kr>
2025-04-20 23:11:59 +0900

    namespace를 이용한 범위 지정

--- 2025_CPP_Study/Namespace.cpp
+++ 2025_CPP_Study/Namespace.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void function() {
-	cout << "A에서 정의한 함수" << endl;
+namespace A_area {
+	void function() {
+		cout << "A에서 정의한 함수" << endl;
+	}
 }
 
-void function() {
-	cout << "B에서 정의한 함수" << endl;
+namespace B_area {
+	void function() {
+		cout << "B에서 정의한 함수" << endl;
+	}
 }
 
 int main() {
-	function();
-	function();
+	A_area::function();
+	B_area::function();
 	return 0;
 }

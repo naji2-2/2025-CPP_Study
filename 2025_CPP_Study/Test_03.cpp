0a1c22fb3f40a8dcb2510ae9ebed9aeae224a01d
Najisu <s2322@e-mirim.hs.kr>
2025-04-23 23:46:11 +0900

    디폴트 값을 주려면 뒤에 있는 변수에 주기

--- 2025_CPP_Study/Test_03.cpp
+++ 2025_CPP_Study/Test_03.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
 // 디폴트 매개변수
-int function(int a = 5, int b) {
-	return a + b +  1;
+// 디폴트 값을 주려면 뒤에 있는 변수에 주기
+int function(int a, int b = 5) {
+	return a + b + 1;
 }
 
 int main(void) {

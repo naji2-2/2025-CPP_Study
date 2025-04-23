58e7c03e9b4fcdb7f754ae2d92c691e210228bd3
Najisu <s2322@e-mirim.hs.kr>
2025-04-24 01:22:36 +0900

    디폴트 함수가 후보함수로 판단되어 오류 발생

--- 2025_CPP_Study/Test_03.cpp
+++ 2025_CPP_Study/Test_03.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 
 // 디폴트 매개변수
-// 디폴트 값을 주려면 뒤에 있는 변수에 주기
-int function(int a, int b = 5) {
-	return a + b + 1;
+int function(int a = 5) {
+	return a + 1;
+}
+
+int function(void) {
+	return 10;
 }
 
 int main(void) {
-	std::cout << function(10, 2) << std::endl;
+	// 두 함수는 다른 매개변수를 가지고 있지만
+	// 디폴트 함수는 둘 다 수행 할 수 있기 때문에
+	// 인자를 넣지 않고 호출했을 때 후보함수로 생각하여 오류가 발생함
+	std::cout << function() << std::endl;	// 실행 할 수 없음
 	std::cout << function(3) << std::endl;
 	return 0;
 }

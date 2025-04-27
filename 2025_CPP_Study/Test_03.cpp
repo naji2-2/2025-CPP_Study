7c64f5b25ae8a4fe2339953436cf60a9426604d0
Najisu <s2322@e-mirim.hs.kr>
2025-04-27 21:58:16 +0900

    해결 방법 2: 이름을 다르게 해서 오버로딩 문제 없애기

--- 2025_CPP_Study/Test_03.cpp
+++ 2025_CPP_Study/Test_03.cpp
@@ -2,8 +2,8 @@
 
 // 디폴트 매개변수
 // 오버로딩 모호성 해결
-// 방법 1: 디폴트 인자를 제거하거나 하나만 정의
-int function(int a) {
+// 방법 2: 이름을 다르게 해서 오버로딩 문제 없애기
+int functionWithParam(int a = 5) {
 	return a + 1;
 }
 
@@ -13,6 +13,6 @@ int function(void) {
 
 int main(void) {
 	std::cout << function() << std::endl;
-	std::cout << function(3) << std::endl;
+	std::cout << functionWithParam(3) << std::endl;
 	return 0;
 }

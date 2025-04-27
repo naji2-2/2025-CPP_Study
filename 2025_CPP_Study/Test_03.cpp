df8beac39dd7d9f05d2842aa0a08ef6f5bc82b7b
Najisu <s2322@e-mirim.hs.kr>
2025-04-27 21:56:01 +0900

    해결 방법 1: 디폴트 인자를 제거하거나 하나만 정의

--- 2025_CPP_Study/Test_03.cpp
+++ 2025_CPP_Study/Test_03.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 
 // 디폴트 매개변수
-int function(int a = 5) {
+// 오버로딩 모호성 해결
+// 방법 1: 디폴트 인자를 제거하거나 하나만 정의
+int function(int a) {
 	return a + 1;
 }
 
@@ -10,10 +12,7 @@ int function(void) {
 }
 
 int main(void) {
-	// 두 함수는 다른 매개변수를 가지고 있지만
-	// 디폴트 함수는 둘 다 수행 할 수 있기 때문에
-	// 인자를 넣지 않고 호출했을 때 후보함수로 생각하여 오류가 발생함
-	std::cout << function() << std::endl;	// 실행 할 수 없음
+	std::cout << function() << std::endl;
 	std::cout << function(3) << std::endl;
 	return 0;
 }
